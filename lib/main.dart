6744a7ed34684fa3df3435004be0c3bc47491e7e
Alex121512 <alexandrurchak@gmail.com>
2023-02-19 02:07:57 +0200

    Change AppBar text

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: App('This is your text'),
+          title: App('This is your first assignment!'),
         ),
         body: OutputText(),
       ),
